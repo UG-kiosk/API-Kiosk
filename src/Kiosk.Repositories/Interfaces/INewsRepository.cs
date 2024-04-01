99de15ad742fa813df4c8b35f436cc542e1c6116
amordzon <a.mordzon.694@studms.ug.edu.pl>
2024-04-01 20:53:25 +0200

    added create news

--- src/Kiosk.Repositories/Interfaces/INewsRepository.cs
+++ src/Kiosk.Repositories/Interfaces/INewsRepository.cs
@@ -9,4 +9,6 @@ public interface INewsRepository
     Task<News?> GetNews(string id, CancellationToken cancellationToken);
 
     Task<(IEnumerable<News>?, Pagination Pagination)> GetManyNews(Source? source, Pagination pagination, CancellationToken cancellationToken);
+    
+    Task CreateNews(IEnumerable<News> news, CancellationToken cancellationToken);
 }
