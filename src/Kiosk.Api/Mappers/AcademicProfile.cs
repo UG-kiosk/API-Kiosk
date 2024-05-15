7cc0a104acf84532f82b486c9004b7ef60ef0b92
jtowarnicka <97166296+jtowarnicka@users.noreply.github.com>
2024-05-15 19:21:12 +0200

    created translation for staff collection (#12)

--- src/Kiosk.Api/Mappers/AcademicProfile.cs
+++ src/Kiosk.Api/Mappers/AcademicProfile.cs
@@ -24,3 +24,36 @@ public class AcademicToListProfile : Profile
             .ForMember(dest => dest.Language, opt => opt.MapFrom((_, _, _, context) => (Language)context.Items["language"]));
     }
 }
+
+public class AcademicRequestProfile : Profile
+{
+    public AcademicRequestProfile()
+    {
+        CreateMap<AcademicRequest, SimplifiedAcademic>()
+            .ForMember(dest => dest.Content, opt => opt.MapFrom(src => new AcademicSimplifiedContent
+            {
+                Posts = src.Content.Posts.Select(p => new AcademicSimplifiedPost
+                {
+                    Position = p.Position,
+                    Faculty = string.Join("; ", p.Faculty)
+                }).ToList(),
+                Tutorial = src.Content.Tutorial
+            }));
+    }
+}
+
+public class AcademicSimplifiedContentProfile : Profile
+{
+    private static readonly string[] Separator = ["; "];
+    public AcademicSimplifiedContentProfile()
+    {
+        CreateMap<AcademicSimplifiedContent, AcademicContent>()
+            .ForPath(dest => dest.Posts, opt => opt.MapFrom(src =>
+                src.Posts.Select(p => new AcademicPost
+                {
+                    Position = p.Position,
+                    Faculty = p.Faculty.Split(Separator, StringSplitOptions.None).ToList(),
+                }).ToList()))
+            .ForPath(dest => dest.Tutorial, opt => opt.MapFrom(src => src.Tutorial));
+    }
+}
--- src/Kiosk.Api/Services/Interfaces/IStaffService.cs
+++ src/Kiosk.Api/Services/Interfaces/IStaffService.cs
@@ -7,6 +7,7 @@ namespace KioskAPI.Services.Interfaces;
 public interface IStaffService
 {  
     Task<(IEnumerable<AcademicToListResponse>, Pagination Pagination)> GetStaff(Language language, int? page, int? itemsPerPage, string? name, CancellationToken cancellationToken);
-    
     Task<AcademicResponse?> GetStaffMember(string academicId, Language language, CancellationToken cancellationToken);
+    Task CreateStaff(IEnumerable<AcademicRequest> staff, CancellationToken cancellationToken);
+    
 }
